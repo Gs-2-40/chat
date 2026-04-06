f06516ab93b346ec3f83e3516ea07f93eaa516ce
Subaev Georgy <subaevgeorgy@gmail.com>
2026-04-07 02:12:07 +0300

    nm

--- app/api/messages.py
+++ app/api/messages.py
@@ -5,7 +5,7 @@ from app.core.config import html
 # Создаем объект роутера
 # prefix — добавится ко всем путям в этом файле
 # tags — поможет сгруппировать методы в документации Swagger (/docs)
-router = APIRouter(prefix="/", tags=["static"])
+router = APIRouter(tags=["static"])
 
 
 @router.get("/ping")
