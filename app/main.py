71de137e17064e9016fd9b87fd36d2dccd038c07
Subaev Georgy <subaevgeorgy@gmail.com>
2026-04-07 02:03:55 +0300

    test .

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from api.auth import router as auth_router
-from websocket import router as ws_router
-from api.messages import router as static_router
+from app.api.auth import router as auth_router
+from app.websocket import router as ws_router
+from app.api.messages import router as static_router
 
 app = FastAPI()
 
